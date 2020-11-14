7d4468e27e148ca8f4a42cd7204f2d5413212cc6
vlad_veko <veko.2001@gmail.com>
2020-11-14 19:10:05 +0300

    Parallel.WaitAll added

---
+++ Parallel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBuffer
+{
+    public static class Parallel
+    {
+        private static TaskQueue taskQueue = new TaskQueue();
+        public static void WaitAll(TaskDelegate[] tasks)
+        {
+            foreach(var task in tasks)
+            {
+                taskQueue.EnqueueTask(task);
+            }
+
+            taskQueue.Stop();
+        }
+    }
+}
